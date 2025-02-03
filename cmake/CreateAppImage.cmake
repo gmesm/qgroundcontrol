514ddeefc3ca0f01a7b667c7b35951c0cb43ade1
Garrett Mesmer <190508517+gmesm@users.noreply.github.com>
2025-02-02 23:09:37 -0500

    Update CreateAppImage.cmake

--- cmake/CreateAppImage.cmake
+++ cmake/CreateAppImage.cmake
@@ -4,18 +4,20 @@ message(STATUS "Creating AppImage")
 set(APPDIR_PATH "${CMAKE_BINARY_DIR}/AppDir")
 set(APPIMAGETOOL_PATH "${CMAKE_BINARY_DIR}/appimagetool-${CMAKE_SYSTEM_PROCESSOR}.AppImage")
 set(LD_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-${CMAKE_SYSTEM_PROCESSOR}.AppImage")
-
 # set(LD_APPIMAGEPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-appimage-${CMAKE_SYSTEM_PROCESSOR}.AppImage")
 # set(LD_QTPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-qt-${CMAKE_SYSTEM_PROCESSOR}.AppImage")
 # set(LD_GSTPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-gstreamer.sh")
 # set(LD_GTKPLUGIN_PATH "${CMAKE_BINARY_DIR}/linuxdeploy-plugin-gtk.sh")
+set(FETCHCONTENT_QUIET FALSE)
 
 if(NOT EXISTS "${APPIMAGETOOL_PATH}")
+    message(STATUS "Downloading appimagetool")
     file(DOWNLOAD https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-${CMAKE_SYSTEM_PROCESSOR}.AppImage "${APPIMAGETOOL_PATH}")
     # file(DOWNLOAD https://github.com/probonopd/go-appimage/releases/download/832/appimagetool-823-x86_64.AppImage "${APPIMAGETOOL_PATH}") # TODO: Use Continuous Release
     execute_process(COMMAND chmod a+x "${APPIMAGETOOL_PATH}")
 endif()
 if(NOT EXISTS "${LD_PATH}")
+    message(STATUS "Downloading linuxdeploy")
     file(DOWNLOAD https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-${CMAKE_SYSTEM_PROCESSOR}.AppImage "${LD_PATH}")
     execute_process(COMMAND chmod a+x "${LD_PATH}")
 endif()
@@ -36,6 +38,7 @@ endif()
 #     execute_process(COMMAND chmod a+x "${LD_GSTPLUGIN_PATH}")
 # endif()
 
+message(STATUS "Executing linuxdeploy")
 execute_process(COMMAND ${LD_PATH}
     --appdir ${APPDIR_PATH}
     --executable ${APPDIR_PATH}/usr/bin/QGroundControl
@@ -46,4 +49,6 @@ execute_process(COMMAND ${LD_PATH}
 
 set(ENV{ARCH} ${CMAKE_SYSTEM_PROCESSOR})
 # set(ENV{VERSION} 5.0)
+
+message(STATUS "Executing appimagetool")
 execute_process(COMMAND ${APPIMAGETOOL_PATH} ${APPDIR_PATH})
